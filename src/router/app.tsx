64241f4e28115338545eeb5cb1bae7ab95ed0f46
Giorgi Koroshinadze <gkoroshinadze@gmail.com>
2020-08-19 01:59:29 +0400

    add material ui provider

--- src/router/app.tsx
+++ src/router/app.tsx
@@ -2,7 +2,7 @@ import React, { useCallback } from 'react';
 import { hot } from 'react-hot-loader/root';
 import { useSelector } from 'react-redux';
 import { Route } from 'react-router-dom';
-import { Switch, Redirect } from 'react-router';
+import { Switch } from 'react-router';
 import { ThemeProvider, DefaultTheme } from 'styled-components';
 
 import { themes } from '@/styled/themes';
@@ -18,6 +18,10 @@ import Login from '@/components/auth/Login';
 import Register from '@/components/auth/Register';
 import { userSelector } from '@/store/ducks/user';
 
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import 'date-fns';
+import DateFnsUtils from '@date-io/date-fns';
+
 const App = () => {
 	const theme = useSelector(appSelectors.theme);
 	const themeSwitch = useActions(appActions.themeSwitchAction);
@@ -32,27 +36,29 @@ const App = () => {
 
 	return (
 		<ThemeProvider theme={activeTheme}>
-			<>
-				<ToastContainer />
-				<GlobalStyle />
-				<Container>
-					<Header changeTheme={changeTheme} isAuthenticated={isAuthenticated} />
-					<Switch>
-						<PublicRoute path="/login" component={Login} appProps={{ isAuthenticated }} />
-						<PublicRoute path="/register" component={Register} appProps={{ isAuthenticated }} />
-						{routes.map((r: IRoute) => (
-							<PrivateRoute
-								key={r.path}
-								path={r.path}
-								exact={r.exact}
-								component={r.component}
-								appProps={{ isAuthenticated }}
-							/>
-						))}
-						<Route component={() => <AsyncPage page="not-found" />} />
-					</Switch>
-				</Container>
-			</>
+			<MuiPickersUtilsProvider utils={DateFnsUtils}>
+				<>
+					<ToastContainer />
+					<GlobalStyle />
+					<Container>
+						<Header changeTheme={changeTheme} isAuthenticated={isAuthenticated} />
+						<Switch>
+							<PublicRoute path="/login" component={Login} appProps={{ isAuthenticated }} />
+							<PublicRoute path="/register" component={Register} appProps={{ isAuthenticated }} />
+							{routes.map((r: IRoute) => (
+								<PrivateRoute
+									key={r.path}
+									path={r.path}
+									exact={r.exact}
+									component={r.component}
+									appProps={{ isAuthenticated }}
+								/>
+							))}
+							<Route component={() => <AsyncPage page="not-found" />} />
+						</Switch>
+					</Container>
+				</>
+			</MuiPickersUtilsProvider>
 		</ThemeProvider>
 	);
 };
